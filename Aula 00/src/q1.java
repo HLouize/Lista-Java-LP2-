caf9dafba9031f9ebd09fbb2182a433ac6e359ff
usuario <usuario@UEPB-00754479>
2026-04-29 15:31:09 -0300

    Primeira Lista

---
+++ Aula 00/src/q1.java
@@ -0,0 +1,22 @@
+public class q1 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+        if (a%2!=0) {
+        	System.out.println("Impar");
+        }
+        else {
+        	System.out.println("Par");
+        }
+        if (b%2!=0) {
+        	System.out.println("Impar");
+        }
+        else {
+        	System.out.println("Par");
+        }
+        
+        
+        
+    }
+}
